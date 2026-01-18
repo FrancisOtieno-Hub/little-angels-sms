f4ee26a7c015aa5bc618b39df32b45b5614108e1
Francis Otieno <info.francisotieno@gmail.com>
2026-01-18 10:06:39 +0300

    Update service-worker.js

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,39 +1,48 @@
-const CACHE_NAME = "little-angels-sms-v1";
-
-const ASSETS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/auth.js",
-  "/pwa/manifest.json"
+const CACHE_NAME = 'laa-sms-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/learners.html',
+  '/fees.html',
+  '/payments.html',
+  '/receipts.html',
+  '/promotion.html',
+  '/css/style.css',
+  '/js/auth.js',
+  '/js/learners.js',
+  '/js/fees.js',
+  '/js/payments.js',
+  '/js/receipts.js',
+  '/js/promotion.js',
+  '/js/supabase.js',
+  '/assets/logo.png'
 ];
 
-// Install
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetch
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
